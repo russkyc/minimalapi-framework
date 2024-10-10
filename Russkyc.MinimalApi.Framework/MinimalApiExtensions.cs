1b9ae0d6cbb7aac9ab0a2c8c239c431af3d20dc7
russkyc <jrcc1023@gmail.com>
2024-10-10 19:13:08 +0800

    :recycle: Move the services related extensions to the ServiceCollectionExtensions class

--- Russkyc.MinimalApi.Framework/MinimalApiExtensions.cs
+++ Russkyc.MinimalApi.Framework/MinimalApiExtensions.cs
@@ -8,55 +8,6 @@ namespace Russkyc.MinimalApi.Framework;
 
 public static class MinimalApiExtensions
 {
-    public static void AddEntityServices<TEntity>(this IServiceCollection serviceCollection,
-        Action<DbContextOptionsBuilder>? optionsAction = null,
-        ServiceLifetime serviceLifetime = ServiceLifetime.Singleton,
-        DatabaseAction databaseAction = DatabaseAction.None) where TEntity : class
-    {
-        serviceCollection.AddDbContextFactory<EntityContext<TEntity>>(optionsAction, serviceLifetime);
-
-        try
-        {
-            using var serviceProvider = serviceCollection.BuildServiceProvider();
-            var contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<EntityContext<TEntity>>>();
-            using var context = contextFactory.CreateDbContext();
-
-            switch (databaseAction)
-            {
-                case DatabaseAction.EnsureCreated:
-                    context.CreateDatabase();
-                    break;
-                case DatabaseAction.DeleteAndCreate:
-                    context.DeleteDatabase();
-                    context.CreateDatabase();
-                    break;
-            }
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Failed to perform database schema updates.");
-        }
-    }
-
-    public static void AddAllEntityServices(this IServiceCollection serviceCollection, Assembly assembly,
-        Action<DbContextOptionsBuilder>? optionsAction = null,
-        ServiceLifetime contextLifetime = ServiceLifetime.Singleton,
-        DatabaseAction databaseAction = DatabaseAction.None)
-    {
-        var entityTypes = assembly
-            .GetTypes()
-            .Where(t => t.GetInterfaces()
-                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IDbEntity<>)));
-
-        foreach (var entityType in entityTypes)
-        {
-            var method = typeof(MinimalApiExtensions).GetMethod(nameof(AddEntityServices))?
-                .MakeGenericMethod(entityType);
-            if (optionsAction != null)
-                method?.Invoke(null, [serviceCollection, optionsAction, contextLifetime, databaseAction]);
-        }
-    }
-
     public static void MapEntityEndpoints<TEntity, TKeyType>(this IEndpointRouteBuilder endpointBuilder,
         string? groupName = null,
         Action<IEndpointConventionBuilder>? routeOptionsAction = null)
---
+++ Russkyc.MinimalApi.Framework/ServiceCollectionExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Russkyc.MinimalApi.Framework.Core;
+
+namespace Russkyc.MinimalApi.Framework;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddEntityServices<TEntity>(this IServiceCollection serviceCollection,
+        Action<DbContextOptionsBuilder>? optionsAction = null,
+        ServiceLifetime serviceLifetime = ServiceLifetime.Singleton,
+        DatabaseAction databaseAction = DatabaseAction.None) where TEntity : class
+    {
+        serviceCollection.AddDbContextFactory<EntityContext<TEntity>>(optionsAction, serviceLifetime);
+
+        try
+        {
+            using var serviceProvider = serviceCollection.BuildServiceProvider();
+            var contextFactory = serviceProvider.GetRequiredService<IDbContextFactory<EntityContext<TEntity>>>();
+            using var context = contextFactory.CreateDbContext();
+
+            switch (databaseAction)
+            {
+                case DatabaseAction.EnsureCreated:
+                    context.CreateDatabase();
+                    break;
+                case DatabaseAction.DeleteAndCreate:
+                    context.DeleteDatabase();
+                    context.CreateDatabase();
+                    break;
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Failed to perform database schema updates.");
+        }
+    }
+
+    public static void AddAllEntityServices(this IServiceCollection serviceCollection, Assembly assembly,
+        Action<DbContextOptionsBuilder>? optionsAction = null,
+        ServiceLifetime contextLifetime = ServiceLifetime.Singleton,
+        DatabaseAction databaseAction = DatabaseAction.None)
+    {
+        var entityTypes = assembly
+            .GetTypes()
+            .Where(t => t.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IDbEntity<>)));
+
+        foreach (var entityType in entityTypes)
+        {
+            var method = typeof(MinimalApiExtensions).GetMethod(nameof(AddEntityServices))?
+                .MakeGenericMethod(entityType);
+            if (optionsAction != null)
+                method?.Invoke(null, [serviceCollection, optionsAction, contextLifetime, databaseAction]);
+        }
+    }
+
+}
